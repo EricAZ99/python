b7a98316d311a430203e46dc592f831a9a2b322f
ericAZ99 <azankpoeric99@gmail.com>
2024-02-05 16:29:16 +0100

    05.02.2024

--- player.py
+++ player.py
@@ -1,24 +1,24 @@
-class Player:
+# class Player:
     
-    ecole = "Nokoué"
+#     ecole = "Nokoué"
     
-    def __init__(self, nom = "Dove", prenom = "ddd", age = 88, taille = ''):
-        self.nom = nom
-        self.prenom = prenom
-        self.age = age
-        self.taille = taille
+#     def __init__(self, nom = "Dove", prenom = "ddd", age = 88, taille = ''):
+#         self.nom = nom
+#         self.prenom = prenom
+#         self.age = age
+#         self.taille = taille
         
-        # print("Je suis {} {}, j'ai {} ans et ma taille est {}".format(nom, prenom, age, taille))
+#         # print("Je suis {} {}, j'ai {} ans et ma taille est {}".format(nom, prenom, age, taille))
     
-    # print("Je suis dans la classe Joueur") 
-    def parler(self, nom, message):
-        print("{} a dit : {}".format(nom, message))
+#     # print("Je suis dans la classe Joueur") 
+#     def parler(self, nom, message):
+#         print("{} a dit : {}".format(nom, message))
         
-    def changement_d_ecole(cls, nouvelle_ecole):
-        Player.ecole = nouvelle_ecole
+#     def changement_d_ecole(cls, nouvelle_ecole):
+#         Player.ecole = nouvelle_ecole
     
-    eleve = classmethod(changement_d_ecole)
+#     eleve = classmethod(changement_d_ecole)
     
-    @staticmethod
-    def definition(message):
-        print("{}".format(message))
+#     @staticmethod
+#     def definition(message):
+#         print("{}".format(message))
